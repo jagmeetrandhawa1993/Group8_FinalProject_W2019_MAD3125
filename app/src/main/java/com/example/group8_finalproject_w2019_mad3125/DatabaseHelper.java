29ca39f6f27b5b59fc912ae89a5e827c9f11204d
gurminderDeol <kaurdeol99@gmail.com>
2019-04-14 18:07:24 -0400

    new changes

---
+++ app/src/main/java/com/example/group8_finalproject_w2019_mad3125/DatabaseHelper.java
@@ -0,0 +1,50 @@
+package com.example.group8_finalproject_w2019_mad3125;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "TechBuy.db";
+    public static final String TABLE_NAME = "User_table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "NAME";
+    public static final String COL_3 = "EMAIL";
+    public static final String COL_4 = "PASSWORD";
+    public static final String COL_6 = "CREDITCARD";
+    public static final String COL_5 = "ADDRESS";
+
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_NAME +" (ID INTEGER PRIMARY KEY AUTOINCREMENT,NAME TEXT,EMAIL TEXT,PASSWORD TEXT,CREDITCARD INTEGER,ADDRESS TEXT)");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean insertData(String name,String email,String password,String Creditcard,String Address) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2,name);
+        contentValues.put(COL_3,email);
+        contentValues.put(COL_4,password);
+        contentValues.put(COL_5,Creditcard);
+        contentValues.put(COL_6,Address);
+
+        long result = db.insert(TABLE_NAME,null ,contentValues);
+        if(result == -1)
+            return false;
+        else
+            return true;
+    }
+}
--- app/src/main/java/com/example/group8_finalproject_w2019_mad3125/Signup.java
+++ app/src/main/java/com/example/group8_finalproject_w2019_mad3125/Signup.java
@@ -1,12 +1,16 @@
 package com.example.group8_finalproject_w2019_mad3125;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Message;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.JsonWriter;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.group8_finalproject_w2019_mad3125.Modal.Users;
 import com.example.group8_finalproject_w2019_mad3125.Utils.JsonUtils;
@@ -21,104 +25,74 @@ import java.util.List;
 
 public class Signup extends AppCompatActivity {
 
-private EditText editTextname;
-private  EditText editTextid;
-    private  EditText editTextpassword;
-    private  EditText editTextaddress;
+    DatabaseHelper myDb;
+
+    private EditText editTextname;
+    private EditText editTextid;
+    private EditText editTextpassword;
+    private EditText editTextaddress;
+    private EditText editTextCardInfo;
     private Button btnsignup;
 
-    String name;
-    String id;
-    String password;
-    String address;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        myDb = new DatabaseHelper(this);
 
-        editTextname = findViewById(R.id.editTextName);
-        editTextid = findViewById(R.id.editTextId);
-        editTextpassword = findViewById(R.id.editTextPassword);
-        editTextaddress = findViewById(R.id.editTextAddress);
+        editTextname = findViewById(R.id.edtName);
+        editTextid = findViewById(R.id.editemail);
+        editTextpassword = findViewById(R.id.edtPassword);
+        editTextaddress = findViewById(R.id.editAddress);
+        editTextCardInfo = findViewById(R.id.editcard);
 
-        btnsignup = findViewById(R.id.btnSignup);
-
-
-
-
-
-        btnsignup.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View v) {
-
-        name=editTextname.getText().toString();
-        id=editTextid.getText().toString();
-        password=editTextpassword.getText().toString();
-        address=editTextaddress.getText().toString();
-
-
-
-
-            JSONObject object = new JSONObject();
-            try {
-                object.put("name", "Jack Hack");
-                object.put("id", "k@k.com");
-                object.put("password", "123");
-                object.put("address", "Punjab");
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            System.out.println(object);
-
+        btnsignup = findViewById(R.id.btnsignup);
+        AddData();
 
 
     }
-});
+    public  void AddData() {
+        btnsignup.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        boolean isInserted = myDb.insertData(editTextname.getText().toString(),
+                                editTextid.getText().toString(),
+                                editTextpassword.getText().toString(),editTextaddress.getText().toString(),editTextCardInfo.getText().toString() );
+                        if(isInserted == true) {
+                            Toast.makeText(Signup.this, "Data Inserted", Toast.LENGTH_LONG).show();
+                            editTextname.setText(" ");
+                            editTextid.setText(" ");
+                            editTextpassword.setText(" ");
+                            editTextaddress.setText(" ");
+                            editTextCardInfo.setText(" ");
+                            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(Signup.this);
+                            alertDialogBuilder.setTitle("Registration Successful");
+                            alertDialogBuilder
+                                    //.setMessage("Click yes to exit!")
+                                   // .setCancelable(false)
+                                    .setPositiveButton("OK",new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog,int id) {
+                                            Intent intent=new Intent(Signup.this, MainActivity.class);
+                                            startActivity(intent);
+                                        }
+                                    });
 
 
+                            // create alert dialog
+                            AlertDialog alertDialog = alertDialogBuilder.create();
 
+                            // show it
+                            alertDialog.show();
+                        }
 
-
+                        else
+                            Toast.makeText(Signup.this,"Data not Inserted",Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
     }
 
-  /*  public void writeJsonStream(OutputStream out, List<Message> messages) throws IOException {
-        JsonWriter writer = new JsonWriter(new OutputStreamWriter(out, "UTF-8"));
-        writer.setIndent("  ");
-        writeMessagesArray(writer, messages);
-        writer.close();
-    }
-
-    public void writeMessagesArray(JsonWriter writer, List<Message> messages) throws IOException {
-        writer.beginArray();
-        for (Message message : messages) {
-            writeMessage(writer, message);
-        }
-        writer.endArray();
-    }
-
-    public void writeMessage(JsonWriter writer, Message message) throws IOException {
-        writer.beginObject();
-        writer.name("name").value(name);
-
-        writer.name("id").value(id);
-        writer.name("password").value(password);
-       writer.name("address").value(address);
-
-
-
-    }
-
-
-
-   public void writeDoublesArray(JsonWriter writer, List<Double> doubles) throws IOException {
-        writer.beginArray();
-        for (Double value : doubles) {
-            writer.value(value);
-        }
-        writer.endArray();
-    }
-
-*/
 }
